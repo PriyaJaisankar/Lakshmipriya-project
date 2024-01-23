ec309c6746a8b5dc5f431f9ce11744c7b4bb5139
Lakshmipriya <jpriya8980@gmail.com>
2024-01-23 11:04:01 +0530

    initial project setup

---
+++ src/main/java/org/Utility/Utility.java
@@ -0,0 +1,6 @@
+package org.Utility;
+
+public class Utility {
+	
+
+}
---
+++ src/test/java/org/Maintest/Test.java
@@ -0,0 +1,7 @@
+package org.Maintest;
+
+public class Test { public static void main(String[] args) {
+	System.out.println("test1");
+}
+
+}
